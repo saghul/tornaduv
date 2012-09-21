0bcac9251b96f39b11aa3735dd7c831c2441e5e3
saghul <saghul@gmail.com>
2012-09-21 23:47:24 +0200

    Added ability to run the Tornado test suite

---
+++ tornado_pyuv/runtests.py
@@ -0,0 +1,88 @@
+
+import logging
+import sys
+
+import tornado_pyuv
+tornado_pyuv.install()
+
+from tornado.test.util import unittest
+
+
+TEST_MODULES = [
+    'tornado.httputil.doctests',
+    'tornado.iostream.doctests',
+    'tornado.util.doctests',
+    'tornado.test.auth_test',
+    'tornado.test.curl_httpclient_test',
+    'tornado.test.escape_test',
+    'tornado.test.gen_test',
+    'tornado.test.httpclient_test',
+    'tornado.test.httpserver_test',
+    'tornado.test.httputil_test',
+    'tornado.test.import_test',
+    'tornado.test.ioloop_test',
+    'tornado.test.iostream_test',
+    #'tornado.test.locale_test',
+    #'tornado.test.log_test',
+    #'tornado.test.options_test',
+    'tornado.test.process_test',
+    'tornado.test.simple_httpclient_test',
+    'tornado.test.stack_context_test',
+    #'tornado.test.template_test',
+    'tornado.test.testing_test',
+    #'tornado.test.twisted_test',
+    'tornado.test.util_test',
+    'tornado.test.web_test',
+    'tornado.test.wsgi_test',
+]
+
+
+def all():
+    return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
+
+
+class TornadoTextTestRunner(unittest.TextTestRunner):
+    def run(self, test):
+        result = super(TornadoTextTestRunner, self).run(test)
+        if result.skipped:
+            skip_reasons = set(reason for (test, reason) in result.skipped)
+            self.stream.write(textwrap.fill(
+                    "Some tests were skipped because: %s" %
+                    ", ".join(sorted(skip_reasons))))
+            self.stream.write("\n")
+        return result
+
+
+if __name__ == '__main__':
+    # The -W command-line option does not work in a virtualenv with
+    # python 3 (as of virtualenv 1.7), so configure warnings
+    # programmatically instead.
+    import warnings
+    # Be strict about most warnings.  This also turns on warnings that are
+    # ignored by default, including DeprecationWarnings and
+    # python 3.2's ResourceWarnings.
+    warnings.filterwarnings("error")
+    # setuptools sometimes gives ImportWarnings about things that are on
+    # sys.path even if they're not being used.
+    warnings.filterwarnings("ignore", category=ImportWarning)
+    # Tornado generally shouldn't use anything deprecated, but some of
+    # our dependencies do (last match wins).
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    warnings.filterwarnings("error", category=DeprecationWarning,
+                            module=r"tornado\..*")
+
+    logging.getLogger("tornado.access").setLevel(logging.CRITICAL)
+
+    import tornado.testing
+    kwargs = {}
+    if sys.version_info >= (3, 2):
+        # HACK:  unittest.main will make its own changes to the warning
+        # configuration, which may conflict with the settings above
+        # or command-line flags like -bb.  Passing warnings=False
+        # suppresses this behavior, although this looks like an implementation
+        # detail.  http://bugs.python.org/issue15626
+        kwargs['warnings'] = False
+    #kwargs['testRunner'] = TornadoTextTestRunner
+    kwargs['verbosity'] = 2
+    tornado.testing.main(**kwargs)
+
