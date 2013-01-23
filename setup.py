d1d4c0795f492f396ac3483cba94f28058dfb0a5
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-01-24 00:43:41 +0100

    Removed classifiers, those for Tornado apply

--- setup.py
+++ setup.py
@@ -10,20 +10,6 @@ setup(
     author_email     = 'saghul@gmail.com',
     description      = 'Tornado IOLoop implementation with pyuv',
     long_description = open('README.rst', 'r').read(),
-    packages         = ['tornado_pyuv'],
-    platforms        = ['POSIX'],
-    classifiers      = [
-          "Development Status :: 3 - Alpha",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: POSIX",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.0",
-          "Programming Language :: Python :: 3.1",
-          "Programming Language :: Python :: 3.2"
-    ]
+    packages         = ['tornado_pyuv']
 )
 
