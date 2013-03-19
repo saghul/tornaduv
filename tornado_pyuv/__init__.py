c4fc339a350e1e44ae4418f53c1a55a44370c319
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-03-19 23:48:01 +0100

    Fixed close method to honor all_fds argument

--- tornado_pyuv/__init__.py
+++ tornado_pyuv/__init__.py
@@ -82,9 +82,12 @@ class IOLoop(object):
         self._loop.walk(cb)
 
     def close(self, all_fds=False):
-        # NOTE: all_fds is disregarded, everything is closed
         self._fdwaker.close()
-        self._handlers = {}
+        for fd in self._handlers.keys():
+            try:
+                os.close(fd)
+            except Exception:
+                logging.debug("error closing fd %s", fd, exc_info=True)
         self._close_loop_handles()
         # Run the loop so the close callbacks are fired and memory is freed
         assert not self._loop.run(pyuv.UV_RUN_NOWAIT), "there are pending handles"
