67caccb28bfb5637a47082ee9df72131590d41c9
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-04-20 12:07:58 +0200

    Don't set version number manually on setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import re
 from distutils.core import setup
 
 
+def get_version():
+    return re.search(r"""__version__\s+=\s+(?P<quote>['"])(?P<version>.+?)(?P=quote)""", open('tornado_pyuv/__init__.py').read()).group('version')
+
 setup(
     name             = 'tornado-pyuv',
-    version          = '0.3.0',
+    version          = get_version(),
     url              = 'https://github.com/saghul/tornado-pyuv',
     author           = 'Saúl Ibarra Corretgé',
     author_email     = 'saghul@gmail.com',
