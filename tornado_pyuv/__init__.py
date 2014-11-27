6c5d0d9a834b95d47ca293c23d2246c0be3fa87a
Marc Schlaich <marc.schlaich@googlemail.com>
2014-11-27 14:59:22 +0100

    Add support for Tornado 4

--- tornado_pyuv/__init__.py
+++ tornado_pyuv/__init__.py
@@ -64,6 +64,8 @@ class UVLoop(IOLoop):
         self._loop = None
 
     def add_handler(self, fd, handler, events):
+        if hasattr(self, 'split_fd'):
+            fd, _ = self.split_fd(fd)
         if fd in self._handlers:
             raise IOError("fd %d already registered" % fd)
         poll = pyuv.Poll(self._loop, fd)
@@ -77,6 +79,8 @@ class UVLoop(IOLoop):
         poll.start(poll_events, self._handle_poll_events)
 
     def update_handler(self, fd, events):
+        if hasattr(self, 'split_fd'):
+            fd, _ = self.split_fd(fd)
         poll = self._handlers[fd]
         poll_events = 0
         if events & IOLoop.READ:
@@ -86,6 +90,8 @@ class UVLoop(IOLoop):
         poll.start(poll_events, self._handle_poll_events)
 
     def remove_handler(self, fd):
+        if hasattr(self, 'split_fd'):
+            fd, _ = self.split_fd(fd)
         poll = self._handlers.pop(fd, None)
         if poll is not None:
             poll.close()
